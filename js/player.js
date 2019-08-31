7b45024038e20eb5d245ffdd952f422171e1174a
SJT1988 <41922761+SJT1988@users.noreply.github.com>
2019-08-31 16:24:41 -0700

    Partitioned classes into their own files
    :D

---
+++ js/player.js
@@ -0,0 +1,52 @@
+//Player class
+class Player extends Populate {
+  constructor () {
+    super();
+    this.x = 0;
+    this.y = 415;
+    this.sprite = "images/char-boy.png";
+    this.lives = 5;
+  }
+
+//key input for Player
+  handleInput (input) {
+    switch (input) {
+      case "left":
+        if (this.x >= this.sideways) {
+          this.x -= this.sideways;
+        }
+        break;
+      case "right":
+        if (this.x <= this.sideways * 3) {
+          this.x += this.sideways;
+        }
+        break;
+      case "up":
+        if (this.y >= 83) {
+          this.y -= this.upDown;
+        }
+        break;
+      case "down":
+        if (this.y <= this.upDown * 4) {
+          this.y += this.upDown;
+        }
+        break;
+    }
+  }
+
+  //updates player and sets condition for collision & win
+  update () {
+    for (let enemy of allEnemies) {
+      if (this.y === enemy.y && (enemy.x + enemy.sideways / 2 > this.x && enemy.x < this.x + this.sideways / 2)) {
+        this.lives--;
+        console.log("Lives left: " + this.lives);
+        this.reset();
+
+        if (this.lives === 0) {
+          //Show game over screen
+          alert("Game over");
+        }
+      }
+    }
+  }
+}
