2c1dd6fcf3a940ac92c87882fa787a1136a5c320
SirHexxus <jamesmichaelstacy@gmail.com>
2019-08-31 17:40:56 -0700

    added title screen overlay

--- js/screens.js
+++ js/screens.js
@@ -1,4 +1,4 @@
 var screenTemplates = { gameOver: '<h1 class="game-over">Game Over</h1><button class="retry-btn">Retry</button>',
 newLevel: '<h1>Level Complete!</h1><h3>Press Any Button To Continue</h3>',
-titleScreen: ''
+titleScreen: '<h1>Welcome to notFROGGER!</h1><h3>Press ENTER to Start</h3>'
 };
